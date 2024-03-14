190e78ae68e86e191019eea6c6aa1a04417d7f90
abhii718 <abhinaysingh47639@gamil.com>
2024-03-14 20:09:35 +0530

    cjs , express, node , js

--- 92/index.js
+++ 92/index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  let siteName = "Adidas";
-  let searchText = "Search Now";
-  res.sendFile("templates/index.html", { root: __dirname });
+  let siteName = "ADIDAS";
+  let searchText = "Search";
+  let arr = [  " abhi" , 2, 3 ]
+  res.render("index", {siteName: siteName, searchText: searchText , arr});
+});
+app.get("/blog/:slug", (req, res) => {
+  let blogTitle = " Adidas why and when ?";
+  let blogContent = "Its a very good brand";
+  res.render("blogpost", {
+    blogTitle: blogTitle,
+    blogContent: blogContent,
+  });
 });
 
 app.listen(port, () => {
