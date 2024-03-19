e7307fd831e42ad85bac81a980ea06e0a2f33822
abhii718 <abhinaysingh47639@gamil.com>
2024-03-19 13:59:19 +0530

    middleware in next.js

---
+++ Script_Link_Image_components/my-app/app/page.js
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+  return <div>
+
+    I am home page
+  
+    </div>;
+}
---
+++ Script_Link_Image_components/my-app/comonents/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+  <nav className='flex justify-between py-4 bg-slate-800 text-white  '>
+    <div className='logo font-bold' >Faebook </div>
+  </nav>
+  )
+}
+
+export default navbar
