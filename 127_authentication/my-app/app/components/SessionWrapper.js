879113acc76523ca72a9dd270c5375ac43e77f44
abhii718 <abhinaysingh47639@gamil.com>
2024-03-19 15:59:09 +0530

    next authentication auth

---
+++ 127_authentication/my-app/app/components/SessionWrapper.js
@@ -0,0 +1,14 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+
+import React from 'react'
+
+const SessionWrapper = ({children}) => {
+  return (
+   <SessionProvider>
+    {children}
+   </SessionProvider>
+  )
+}
+
+export default SessionWrapper
